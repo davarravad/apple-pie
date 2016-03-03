c890336de853af905b5c0f93346496744641c23b
Bryan Y <cs337bryan@gmail.com>
2016-03-03 15:52:03 -0800

    test

--- app/Modules/Members/Controllers/Members.php
+++ app/Modules/Members/Controllers/Members.php
@@ -130,6 +130,7 @@ class Members extends Controller
                         $check = getimagesize ( $picture['tmp_name'] );
 
                         if($picture['size'] < 1000000 && $check && $check->type == "image/jpeg"){
+                            var_dump(file_exists(DIR.'images/profile-pics'));
                             mkdir(DIR.'images/profile-pics');
                             $image = new SimpleImage($picture['tmp_name']);
                             $image->best_fit(400,300)->save(DIR.'images/profile-pics/'.$username.'.jpg');
