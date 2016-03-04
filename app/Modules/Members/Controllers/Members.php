783de38016c31dfdf8a98da28d5c4e4fa51fb248
Bryan Y <cs337bryan@gmail.com>
2016-03-03 16:27:01 -0800

    test

--- app/Modules/Members/Controllers/Members.php
+++ app/Modules/Members/Controllers/Members.php
@@ -128,7 +128,7 @@ class Members extends Controller
                     if(sizeof($picture)>0){
                         var_dump($picture);
                         $check = getimagesize ( $picture['tmp_name'] );
-                        var_dump($picture['size']);
+                        var_dump($check['type']);
                         if($picture['size'] < 1000000 && $check && $check['type'] == "image/jpeg"){
                             //var_dump($check);
                             var_dump("--------");
