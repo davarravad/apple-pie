45a15de6b2a21a54c9ac38c6b387974312f93940
Bryan Y <cs337bryan@gmail.com>
2016-03-09 18:29:43 -0800

    more

--- app/Helpers/Auth/Auth.php
+++ app/Helpers/Auth/Auth.php
@@ -358,7 +358,7 @@ class Auth {
                         //todo bien continua con register
                         $password = $this->hashPass($password);
                         $activekey = $this->randomKey(RANDOM_KEY_LENGTH); //genera una randomkey para activacion enviar por email
-                        $info = array("username" => $username, "password" => $password, "email" => $email, "activekey" => $activekey, "userImage"=>"images/profile-pics/defaults/default-".rand(1,5));
+                        $info = array("username" => $username, "password" => $password, "email" => $email, "activekey" => $activekey, "userImage"=>"images/profile-pics/defaults/default-".rand(1,5).".jpg");
                         $user_id = $this->authorize->addIntoDB("users", $info);
 
                         $info = array('userID' => $user_id, 'groupID' => 1);
@@ -441,7 +441,7 @@ class Auth {
                         // Email address isn't already used
                         $password = $this->hashPass($password);
                         $activekey = $this->randomKey(RANDOM_KEY_LENGTH);
-                        $info = array("username" => $username, "password" => $password, "email" => $email, "activekey" => $activekey, "userImage"=>"images/profile-pics/defaults/default-".rand(1,5));
+                        $info = array("username" => $username, "password" => $password, "email" => $email, "activekey" => $activekey, "userImage"=>"images/profile-pics/defaults/default-".rand(1,5).".jpg");
                         $user_id = $this->authorize->addIntoDB("users", $info);
 
                         $info = array('userID' => $user_id, 'groupID' => 1);
